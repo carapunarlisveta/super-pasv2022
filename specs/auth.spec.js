145236ab62a8100abd70f777e0408d7b9fc99945
carapunarlisveta <98380314+carapunarlisveta@users.noreply.github.com>
2022-03-05 14:01:41 -0800

    First api tests (#4)
    * add babel support
    
    * add auth tests
    
    * add auth tests
    
    * add auth tests
    
    * add auth tests
    
    * add environment variables supprt

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,5 +1,14 @@
 import supertest from 'supertest'
 import { expect } from 'chai'
+import 'dotenv/config'
+
+describe('Auth', function() {
+    it('Successful log in', function() {
+    const request = supertest(process.env.BASE_URL)
+        request
+            .post('/auth')
+            .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
+            .end(function(err, res) {
 
 describe('Auth ', function() {
     it('Successful log in', function () {
@@ -7,10 +16,23 @@ describe('Auth ', function() {
             .post('/auth')
             .send({login: 'adminius', password: 'supers3cret'})
             .end(function (err, res) {
+
                 expect(res.statusCode).to.eq(200)
                 expect(res.body.token).not.to.be.undefined
             })
     })
+
+    it('Log in with invalid credentials', function() {
+        request
+            .post('/auth')
+            .send({ login: 'invalid', password: 'invalid' })
+            .end(function(err, res) {
+                expect(res.statusCode).to.eq(404)
+                expect(res.body.message).to.eq('Wrong login or password')
+            })
+
+    })
+
     it('Log in with invalid credentials', function () {
         const request = supertest('https://paysis.herokuapp.com')
         request
