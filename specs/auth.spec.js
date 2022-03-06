298762858642b06a621fd9209d633f88d0270e1f
svetlanacarapunarli <carapunarlisveta@gmail.com>
2022-03-05 17:10:09 -0800

    add before hooks for auth spec add root before hook

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,64 +1,50 @@
 import supertest from 'supertest'
 import { expect } from 'chai'
-import 'dotenv/config'
+
 
 describe('Auth ', function() {
     const request = supertest(process.env.BASE_URL)
 
-    it('Successful log in', function () {
-        request
-            .post('/auth')
-            .send({login: process.env.LOGIN, password: process.env.PASSWORD})
-            .end(function (err, res) {
+    describe('Successful log in', function() {
+        let result
 
-describe('Auth', function() {
-    const request = supertest(process.env.BASE_URL)
+        before(async function() {
+            await request
+                .post('/auth')
+                .send({login: process.env.LOGIN, password: process.env.PASSWORD})
+                .then(res=> {
+                    result = res
+                })
+        })
+        it('response status code is 200', function () {
+                    expect(result.statusCode).to.eq(200)
 
-    it('Successful log in', function() {
-        request
-            .post('/auth')
-            .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-            .end(function(err, res) {
+                })
+        it('response status code is 200', function () {
+            expect(result.body.token).not.to.be.undefined
+        })
+        })
 
-describe('Auth ', function() {
-    it('Successful log in', function () {
-        request
-            .post('/auth')
-            .send({login: 'adminius', password: 'supers3cret'})
-            .end(function (err, res) {
-          
-                expect(res.statusCode).to.eq(200)
-                expect(res.body.token).not.to.be.undefined
-            })
-    })
+    describe('Log in with invalid credentials', function() {
+        let result
 
-    it('Log in with invalid credentials', function () {
+        before( async function() {
+            await request
+                .post('/auth')
+                .send({login: 'invalid', password: 'invalid'})
+                .then(res=> {
+                    result = res
+                })
+        })
 
+        it('response status code is 404', function () {
+                    expect(res.statusCode).to.eq(404)
+                })
 
-    it('Log in with invalid credentials', function() {
-        request
-            .post('/auth')
-            .send({ login: 'invalid', password: 'invalid' })
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(404)
-                expect(res.body.message).to.eq('Wrong login or password')
-            })
-
-    })
-
-    it('Log in with invalid credentials', function () {
-        const request = supertest('https://paysis.herokuapp.com')
-
-        request
-            .post('/auth')
-            .send({login: 'invalid', password: 'invalid'})
-            .end(function (err, res) {
-                expect(res.statusCode).to.eq(404)
-                expect(res.body.token).to.eq('Wrong login or password.')
-            })
+        it('response status code is 404', function () {
+            expect(res.body.token).to.eq('Wrong login or password.')
+        })
     })
 
 })
-
-}
 
