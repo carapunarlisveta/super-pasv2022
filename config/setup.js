10b665b80f5468d759ce373b27a776005bdd527b
svetlanacarapunarli <carapunarlisveta@gmail.com>
2022-03-06 23:06:04 -0800

    add users spec add users helper add common helper add config helper

--- config/setup.js
+++ config/setup.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import AuthHelper from "../helpers/auth.helper";
+import AuthHelper from '../helpers/auth.helper'
 
     before(async function() {
         const authHelper = new AuthHelper()
